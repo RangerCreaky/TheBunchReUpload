5e0ceb32b7ca48630144fcf77045f8d7e301f1e2
Navaneeth <navaneeth.penumarthi@gmail.com>
2022-08-03 20:07:48 +0530

    lets see

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 // serve static assets in production
-if (PROCESS.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     //set static folder
     app.use(express.static("client/build"));
 
